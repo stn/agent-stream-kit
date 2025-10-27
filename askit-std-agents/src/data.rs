f191d95dfe02fa2a28bd397237ba5378e45d9e2c
Akira Ishino <akira@lumilab.jp>
2025-10-27 21:28:06 +0900

    ToYamlAgent and FromYamlAgent

--- askit-std-agents/src/data.rs
+++ askit-std-agents/src/data.rs
@@ -78,6 +78,79 @@ impl AsAgent for FromJsonAgent {
     }
 }
 
+// To YAML
+struct ToYamlAgent {
+    data: AsAgentData,
+}
+
+#[async_trait]
+impl AsAgent for ToYamlAgent {
+    fn new(
+        askit: ASKit,
+        id: String,
+        def_name: String,
+        config: Option<AgentConfig>,
+    ) -> Result<Self, AgentError> {
+        Ok(Self {
+            data: AsAgentData::new(askit, id, def_name, config),
+        })
+    }
+
+    fn data(&self) -> &AsAgentData {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut AsAgentData {
+        &mut self.data
+    }
+
+    async fn process(&mut self, ctx: AgentContext, data: AgentData) -> Result<(), AgentError> {
+        let yaml = serde_yaml_ng::to_string(&data.value)
+            .map_err(|e| AgentError::InvalidValue(e.to_string()))?;
+        self.try_output(ctx, PORT_YAML, AgentData::string(yaml))?;
+        Ok(())
+    }
+}
+
+// From YAML
+struct FromYamlAgent {
+    data: AsAgentData,
+}
+
+#[async_trait]
+impl AsAgent for FromYamlAgent {
+    fn new(
+        askit: ASKit,
+        id: String,
+        def_name: String,
+        config: Option<AgentConfig>,
+    ) -> Result<Self, AgentError> {
+        Ok(Self {
+            data: AsAgentData::new(askit, id, def_name, config),
+        })
+    }
+
+    fn data(&self) -> &AsAgentData {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut AsAgentData {
+        &mut self.data
+    }
+
+    async fn process(&mut self, ctx: AgentContext, data: AgentData) -> Result<(), AgentError> {
+        let s = data
+            .value
+            .as_str()
+            .ok_or_else(|| AgentError::InvalidValue("not a string".to_string()))?;
+        let value: serde_json::Value =
+            serde_yaml_ng::from_str(s).map_err(|e| AgentError::InvalidValue(e.to_string()))?;
+        let data = AgentData::from_json(value)?;
+        self.try_output(ctx, PORT_DATA, data)?;
+        Ok(())
+    }
+}
+
 // Get Property
 struct GetPropertyAgent {
     data: AsAgentData,
@@ -168,6 +241,7 @@ static CATEGORY: &str = "Core/Data";
 
 static PORT_DATA: &str = "data";
 static PORT_JSON: &str = "json";
+static PORT_YAML: &str = "yaml";
 
 static CONFIG_PROPERTY: &str = "property";
 
@@ -193,6 +267,30 @@ pub fn register_agents(askit: &ASKit) {
         .with_title("From JSON")
         .with_category(CATEGORY)
         .with_inputs(vec![PORT_JSON])
+        .with_outputs(vec![PORT_DATA]),
+    );
+
+    askit.register_agent(
+        AgentDefinition::new(
+            AGENT_KIND,
+            "std_to_yaml",
+            Some(new_agent_boxed::<ToYamlAgent>),
+        )
+        .with_title("To YAML")
+        .with_category(CATEGORY)
+        .with_inputs(vec![PORT_DATA])
+        .with_outputs(vec![PORT_YAML]),
+    );
+
+    askit.register_agent(
+        AgentDefinition::new(
+            AGENT_KIND,
+            "std_from_yaml",
+            Some(new_agent_boxed::<FromYamlAgent>),
+        )
+        .with_title("From YAML")
+        .with_category(CATEGORY)
+        .with_inputs(vec![PORT_YAML])
         .with_outputs(vec![PORT_DATA]),
     );
 
